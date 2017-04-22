08a348e0276d0312b4eb6413f01c2bf6f43b549b
x1212 <x1212-x@web.de>
2017-04-22 22:11:29 +0200

    upgrading seems to work

--- ui/ui.gd
+++ ui/ui.gd
@@ -10,7 +10,13 @@ func _ready():
 func _process(delta):
 	var assigned = get_parent().get_assigned()
 	var assigned_goal = get_parent().get_assigned_goal()
+	var cost = get_parent().get_levelup_cost()
 	get_node("Label").set_text("assigned Bees: " + str(assigned) + "\nrequested Bees: " + str(assigned_goal) )
+	get_node("cost").set_text("upgrade costs: " + str(cost))
+	if (cost > get_tree().get_root().get_node("/root/game_state").material):
+		get_node("cost").set("custom_colors/font_color", Color(1,0,0))
+	else:
+		get_node("cost").set("custom_colors/font_color", Color(1,1,1))
 
 
 func _on_minus_pressed():
@@ -21,3 +27,10 @@ func _on_minus_pressed():
 func _on_plus_pressed():
 	if ( get_parent().get_assigned_goal() < get_parent().get_assignment_max() ):
 		get_parent().goal_assign()
+
+
+func _on_levelup_pressed():
+	var cost = get_parent().get_levelup_cost()
+	if (cost <= get_tree().get_root().get_node("/root/game_state").material):
+		get_tree().get_root().get_node("/root/game_state").material -= cost
+		get_parent().level += 1
